4a43685c23712ced7790755179cce635328d1bb5
shenyinghao <191728786@qq.com>
2016-09-16 11:28:49 +0800

    Initial Commit

---
+++ TableviewDemol/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TableviewDemol
+//
+//  Created by shenyinghao on 16/9/16.
+//  Copyright © 2016年 shenyinghao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TableviewDemol/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TableviewDemol
+//
+//  Created by shenyinghao on 16/9/16.
+//  Copyright © 2016年 shenyinghao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
