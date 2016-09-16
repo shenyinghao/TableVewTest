13c4da61c92c058eaea1ced308a0ca030719c93a
shenyinghao <191728786@qq.com>
2016-09-16 15:47:56 +0800

    TableViewTest

--- TableviewDemol/ViewController.m
+++ TableviewDemol/ViewController.m
@@ -8,7 +8,14 @@
 
 #import "ViewController.h"
 
+#import "DataSource.h"
+
 @interface ViewController ()
+
+@property (nonatomic, strong) UITableView *tableview;
+
+/// 这个代理不能是临时变量，否则tableview会出现bug
+@property (nonatomic, strong) DataSource *photosArrayDataSource;
 
 @end
 
@@ -16,8 +23,23 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.navigationItem.title = @"tableview";
+    [self setupTableView];
     // Do any additional setup after loading the view, typically from a nib.
 }
+
+
+- (void)setupTableView
+{
+    /// 数据源可以多样化，不只是字符串，其他类型也可以
+    NSArray *photos = @[@"dadff",@"wefdsgfd"];
+    self.photosArrayDataSource = [[DataSource alloc] initWithItems:photos];
+    _tableview = [[UITableView alloc] initWithFrame:self.view.bounds style:UITableViewStylePlain];
+    self.tableview.delegate = self.photosArrayDataSource;
+    self.tableview.dataSource = self.photosArrayDataSource;
+    [self.view addSubview:_tableview];
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
